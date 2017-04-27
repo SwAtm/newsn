42fced777f6c98d387fd054788615cfe62e163f8
root <root@localhost.localdomain>
2017-04-27 22:57:08 +0530

    Added few files

---
+++ application/views/fitness/get_id.php
@@ -0,0 +1,12 @@
+<?php
+echo validation_errors();
+$template=array('table_open'=>'<table border=1 align=center width=50%>');
+$this->table->set_template($template);
+$this->table->set_heading(array('data'=>'Form to record fitness', 'align'=>'center'));
+echo form_open('fitness/get_details_id');
+$this->table->add_row(form_label('Enter ID','id'));
+$this->table->add_row(form_input('id'));
+$this->table->add_row(form_submit('submit','Submit'));
+echo $this->table->generate();
+echo form_close();
+?>
---
+++ application/views/home/menu1.php
@@ -0,0 +1,5 @@
+<?php
+echo "<table width=100%><tr><td align=middle><a href=".site_url('patients/search').">Search Patient</a href></td></tr>";
+echo "<tr><td align=center><a href=".site_url('fitness/get_oid').">Record Fitness</a href></td></tr>";
+echo "<tr><td align=center><a href=".site_url('fitness/get_date').">List Fitness</a href></td></tr></table>";
+?>
