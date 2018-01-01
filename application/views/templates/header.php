82f5641b5f47b78360d08a126c620d965bacd681
root <root@mx1>
2018-01-01 13:26:55 +0530

    Added style to outline selected field in header file.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,3 +1,9 @@
+<style type="text/css">
+input:focus, select:focus, checkbox:focus {
+      //background: #fc9fff;   
+      outline:3px solid magenta;
+}
+</style>
 <?php
 echo "<table width=100% border=2 bgcolor=lightblue cellpadding=15>";
 echo "<tr><td colspan=2 align=middle><b>Sarada Netralaya</b></td></tr></table>";
