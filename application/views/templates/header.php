d65812e851f468b380c85a6c9975c4f9b33d4a2d
root <root@mx1>
2019-11-20 12:07:39 +0530

    Disabled back button by adding code to views/templates/header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,11 @@ input:focus, select:focus, checkbox:focus {
       outline:3px solid magenta;
 }
 </style>
+<script type = "text/javascript" >
+   function preventBack(){window.history.forward();}
+    setTimeout("preventBack()", 0);
+    window.onunload=function(){null};
+</script>
 <?php
 echo "<table width=100% border=2 bgcolor=lightblue cellpadding=15>";
 echo "<tr><td colspan=2 align=middle><b>Sarada Netralaya</b></td></tr></table>";
